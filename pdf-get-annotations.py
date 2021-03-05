69746071984c5a21712b09f5c1fd896c5a2fe1c8
Błażej Niewiadomski <blaz.nie@protonmail.com>
2021-03-05 05:02:12 +0100

    pdf script

---
+++ pdf-get-annotations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+import PyPDF2, traceback
+
+try:
+    src = sys.argv[1]
+except:
+    src = r'/path/to/my/file.pdf'
+
+try:
+    path = sys.argv[2]
+    out = open(path, "wt")
+except:
+    out = sys.stdout
+
+    
+
+input1 = PyPDF2.PdfFileReader(open(src, "rb"))
+nPages = input1.getNumPages()
+
+for i in range(nPages):
+    page = input1.getPage(i)
+    try:
+        for annot in page['/Annots']:
+            annotObject = annot.getObject()
+            # pages start at zero so they number is 1 behind real ones
+            out.write('page number: {}\n'.format(input1.getPageNumber(page)+1))
+            out.write('contents:\n{}\n\n'.format(annotObject['/Contents']))
+    except: 
+        # there are no annotations on this page
+        pass
